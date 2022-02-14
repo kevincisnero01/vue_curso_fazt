9649984c80e5b4498a0ddde512d7d562580d52ea
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2022-02-14 16:04:58 -0430

    Clase 5
    Métodos
     v-on:click=”saludar”
     v-on:click=”saludar2( parámetro )”
     v-model: doble enlace de datos binding
     v-on:keyup=”typing”
     Validar si presionan un “enter”

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,58 @@
 <template>
   <div id="app">
-    <h1 v-text="msg"></h1>
-    <img src="./assets/logo.png">
-    <hr>
-    <div v-html="contentHTML"></div>
-    <h2 v-html="bucleTitle"></h2>
-    <ul>
-      <li v-for="skill in skills"> {{ skill.name }}</li>
-    </ul>
-    <p v-if="msgShow">Contenido Visible(v-if)</p>
+    <h1>Clase 5</h1>
+    <fieldset class="car">
+      <legend>Evento: v-on:click="saludar()"</legend>
+      <button type="button" v-on:click="saludar">Click Me </button>
+    </fieldset>
 
-    <p v-if="!msgHidden">Contenido Oculto (v-else)</p>
+    <fieldset class="car">
+      <legend>Evento: v-on:click="saludar(parametro)"</legend>
+      <button type="button" v-on:click="saludar2(user.lastName)">Click Me</button>
+    </fieldset>
 
-    <test></test>
+    <fieldset class="car">
+      <legend>Evento: v-model & v-on:click="saludar()"</legend>
+      <input type="text" v-model="user.firstName">
+      <button type="button" v-on:click="saludar2(user.firstName)">Click Me</button>
+    </fieldset>
+
+    <fieldset class="car">
+      <legend>Evento: v-on:keyup="typing" </legend>
+      <input type="text" v-on:keyup="typing">
+    </fieldset>
+
+    <fieldset class="car">
+      <legend>Evento: v-on:keyup.enter="typedEnter"</legend>
+      <input type="text" v-on:keyup.enter="typedEnter">
+    </fieldset>
+
   </div>
 </template>
 
 <script>
-import Test from './components/Test.vue'
 
 export default {
-  name: 'app',
-  components:{
-    Test
-  },
   data () {
     return {
-      msg: 'Bienvenidos a tu Vue.js App (v-text)',
-      contentHTML: '<h2>HTML(v-html)</h2>',
-      bucleTitle: 'Lista de Habilidades(v-for)',
-      msgShow: true,
-      msgHidden: false,
-      skills: [
-      { name:'Lectura'},
-      { name:'Ejercicio'},
-      { name:'Caminar'}
-      ]
+      user: {
+        firstName: 'Kevin',
+        lastName: 'Cisnero'
+      }
+    }
+  },
+  methods: {
+    saludar: function(){
+      alert('Hola ' + this.user.firstName)
+    },
+    saludar2: function(name){
+      alert('Hola ' + name)
+    },
+    typing: function(e){
+      console.log(e.target.value)
+    },
+    typedEnter: function(e){
+      console.log('Se persiono Enter')
     }
   }
 }
@@ -46,9 +63,11 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding: 10px;
+  border:1px solid #d2d2d2;
+  border-radius: 5px;
 }
 h1, h2 {
   font-weight: normal;
@@ -66,5 +85,11 @@ li {
 
 a {
   color: #42b983;
+}
+
+.car{
+  display: inline-block;
+  padding: 10px;
+  width: 30%;
 }
 </style>
